0c44cf1bc0f73194ce58c5d412bcf3d1e06460ba
Ghassan Malke <138721734+gma1k@users.noreply.github.com>
2023-07-25 08:11:26 +0200

    Create tables_schemas.sql (#43)

---
+++ mysql/tables_schemas.sql
@@ -0,0 +1,2 @@
+SELECT table_name, table_schema FROM information_schema.tables WHERE table_schema = 'sugarcrm';
+SELECT table_name, view_definition FROM information_schema.views WHERE table_schema = 'sugarcrm';
