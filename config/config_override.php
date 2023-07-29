aee680743e3366d5da33e7975c8c09b64eb737e0
Ghassan Malke <138721734+gma1k@users.noreply.github.com>
2023-07-29 23:29:51 +0200

    Create config_override.php

---
+++ config/config_override.php
@@ -0,0 +1,10 @@
+<?php
+
+$sugar_config = array (
+  'maintenanceMode' => true,
+  'default_theme' => 'RacerX',
+  'default_date_format' => 'd/m/Y',
+  'default_currency_iso4217' => 'EUR',
+  'default_currency_symbol' => '€',
+  'default_language' => 'en_us',
+);
